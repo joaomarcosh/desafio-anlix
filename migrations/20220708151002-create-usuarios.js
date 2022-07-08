61315137cbabab6fb4ddbca6383300fde110fa38
TimeGuardian <joaomarcosh@gmail.com>
2022-07-08 11:19:22 -0400

    Add Table Usuarios

---
+++ migrations/20220708151002-create-usuarios.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Usuarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      usuario: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      senha: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      cargo: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Usuarios');
+  }
+};
